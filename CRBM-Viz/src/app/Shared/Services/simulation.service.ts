a4aa659a4a347d50d75e33d827e9eb1f30f15b9c
gmarupilla <marupilla@uchc.edu>
2020-01-10 11:05:26 -0500

    Remove dead code from simulation service

--- CRBM-Viz/src/app/Shared/Services/simulation.service.ts
+++ CRBM-Viz/src/app/Shared/Services/simulation.service.ts
@@ -26,12 +26,7 @@ import { User } from '../Models/user';
 })
 export class SimulationService {
 
-  simulationData: object = null;
-  fileData: Array<object> = null;
-  omexFiles: Array<string> = null;
-  solverFiles: Array<string> = null;
-  sbatchFiles: Array<string> = null;
-  simulationDataChangeSubject = new Subject<null>();
+  simulationDataChangeSubject = new Subject<object>();
 
   private userService: UserService;
 
@@ -215,69 +210,9 @@ export class SimulationService {
     }
   }
 
-  getSimulationAndJobFilesInfo(): void {
-    this.http.get(`${environment.crbm.CRBMAPI_URL}/simulation`)
-    .subscribe(
-      success => {
-        this.simulationData = this.flattenSimulationData(
-          success['data']['simulations']
-        );
-        this.omexFiles = success['data']['omexSolvers']['omex'];
-        this.solverFiles = success['data']['omexSolvers']['solver'];
-        const sbatches = []
-          for (const sbatch of success['data']['files']) {
-            sbatches.push(`${sbatch['createdBy']}-${sbatch['filename']}`);
-          }
-        this.sbatchFiles = sbatches;
-        this.fileData = success['data']['files']
-        this.simulationDataChangeSubject.next();
-      },
-      error => {
-        this.alertService.openDialog(
-          'Error occured in Simulation service: ' +
-          JSON.stringify(error)
-        );
-      }
-    );
-  }
 
-  createSimulation(
-    selectedSbatch: string,
-    selectedOmex: string,
-    selectedSolver: string
-    ) {
-    const id = this.getFileId(selectedSbatch);
-    return this.http.post(`${environment.crbm.CRBMAPI_URL}/simulation`, {
-      omex: selectedOmex,
-      solver: selectedSolver,
-      fileId: id
-    });
-  }
 
-  getFileId(selectedSbatch: string) {
-    const fileSplitted = selectedSbatch.split('-');
-    const user = fileSplitted[0];
-    const filename = fileSplitted[1];
-    const fileObj = this.fileData.find(
-        file => file['createdBy'] === user && file['filename'] === filename
-      );
-    return fileObj['fileId'];
-  }
 
-  flattenSimulationData(simData) {
-    const data = [];
-    for (const sim of simData) {
-      const simObj = { ...sim };
-      const jobInfo = sim['jobInfo'];
-      for (const key in jobInfo) {
-        if (jobInfo.hasOwnProperty(key)) {
-          simObj[key] = jobInfo[key];
-        }
-      }
-      data.push(simObj);
-    }
-    return data;
-  }
 
   get(id: string): Simulation {
     this.getServices();
@@ -295,14 +230,6 @@ export class SimulationService {
     return this.filter(data, name) as Simulation[];
   }
 
-  private filter(list: object[], name?: string): object[] {
-    if (name) {
-      const lowCaseName: string = name.toLowerCase();
-      return list.filter(item => item['name'].toLowerCase().includes(lowCaseName));
-    } else {
-      return list;
-    }
-  }
 
   getHistory(id: string, includeParents: boolean = true, includeChildren: boolean = true): object[] {
     // tslint:disable:max-line-length
